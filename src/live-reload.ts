eeb875735b78c4aab9c6a62bcfdfbe54c0f033b7
Robin Boers <robindev2019@outlook.com>
2023-09-05 14:47:25 +0200

    Setup p5js

--- src/live-reload.ts
+++ src/live-reload.ts
@@ -1,4 +1,7 @@
 // This snippet enables live reloading via the ESBuild API.
 // See https://esbuild.github.io/api/#live-reload
 
-new EventSource('/esbuild').addEventListener('change', () => location.reload())
+export function setup_live_reload() {
+  new EventSource("/esbuild").addEventListener("change", () => location.reload());
+};
+
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,25 @@
-import "./live-reload";
+import p5 from "p5";
+import { setup_live_reload } from "./live-reload";
 
-// Source code will go here.
-console.log("Hello world");
+function setup() {
+  this.createCanvas(400, 400);
+}
+
+function draw() {
+  if (this.mouseIsPressed) {
+    this.fill(0);
+  } else {
+    this.fill(255);
+  }
+  this.ellipse(this.mouseX, this.mouseY, 80, 80);
+}
+
+new p5((sketch) => {
+  sketch.setup = () => {
+    setup.apply(sketch, []);
+    setup_live_reload();
+  };
+  sketch.draw = () => {
+    draw.apply(sketch, []);
+  };
+}, "p5-canvas");
