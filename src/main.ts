8596a6a8b44224b910b2c8bc6a4f5afce7703ea9
MSB246 <msembulut@gmail.com>
2023-09-05 15:56:25 +0200

    Added simple vertex-faces example

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,47 @@
 import p5 from "p5";
 import { setup_live_reload } from "./live-reload";
 
+let m;
+
 function setup() {
-  this.createCanvas(400, 400);
+  this.createCanvas(400, 400, this.WEBGL);
+  m = createModel();
 }
 
 function draw() {
-  if (this.mouseIsPressed) {
-    this.fill(0);
-  } else {
-    this.fill(255);
-  }
-  this.ellipse(this.mouseX, this.mouseY, 80, 80);
+    this.clear();
+    this.orbitControl();
+    this.model(m);
+}
+
+function createModel() {
+  return new p5.Geometry(
+    1, 1,
+    function createGeometry() {
+      this.vertices.push(
+        new p5.Vector(-50, -50, 50),
+        new p5.Vector(50, -50, 50),
+        new p5.Vector(-50, 50, 50),
+        new p5.Vector(50, 50, 50),
+
+        new p5.Vector(-50, -50, -50),
+        new p5.Vector(50, -50, -50),
+        new p5.Vector(-50, 50, -50),
+        new p5.Vector(50, 50, -50),
+      );
+      this.faces.push(
+          [0, 1, 3],
+          [0, 2, 3],
+
+          [4, 5, 7],
+          [4, 6, 7],
+
+          [0, 4, 6],
+          [0, 2, 6],
+          [0, 2, 3],
+      );
+    }
+  );
 }
 
 new p5((sketch) => {
