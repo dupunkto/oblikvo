50fd26d99414da53f2ae92ed230bb963268105a5
Robin Boers <robindev2019@outlook.com>
2023-09-25 15:53:33 +0200

    Use class structure instead of random functions and global variables

--- src/live-reload.ts
+++ src/live-reload.ts
@@ -1,7 +1,7 @@
 // This snippet enables live reloading via the ESBuild API.
 // See https://esbuild.github.io/api/#live-reload
 
-export function setup_live_reload() {
+export default function setup_live_reload() {
   new EventSource("/esbuild").addEventListener("change", () => location.reload());
 };
 
---
+++ src/radiotopian.ts
@@ -0,0 +1,61 @@
+export default class Radiotopian {
+  constructor(sketch) {
+    this.sketch = sketch;
+  }
+
+  setup() {
+    this.setupCanvas();
+    this.model = this.createModel();
+  }
+
+  setupCanvas() {
+    this.sketch.createCanvas(
+      document.body.clientWidth, 
+      document.body.clientHeight, 
+      this.sketch.WEBGL
+    ); 
+
+    window.addEventListener("resize", function(e) {
+      this.sketch.resizeCanvas(
+        document.body.clientWidth, 
+        document.body.clientHeight
+      );
+    });
+  }
+
+  createModel() {
+    return new p5.Geometry(
+      1, 1,
+      function createGeometry() {
+        this.vertices.push(
+          new p5.Vector(-50, -50, 50),
+          new p5.Vector(50, -50, 50),
+          new p5.Vector(-50, 50, 50),
+          new p5.Vector(50, 50, 50),
+
+          new p5.Vector(-50, -50, -50),
+          new p5.Vector(50, -50, -50),
+          new p5.Vector(-50, 50, -50),
+          new p5.Vector(50, 50, -50),
+        );
+        this.faces.push(
+            [0, 1, 3],
+            [0, 2, 3],
+
+            [4, 5, 7],
+            [4, 6, 7],
+
+            [0, 4, 6],
+            [0, 2, 6],
+            [0, 2, 3],
+        );
+      }
+    );
+  }
+
+  draw() {
+    this.sketch.clear();
+    this.sketch.orbitControl();
+    this.sketch.model(this.model);
+  }
+}
