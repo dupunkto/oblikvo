3c79634f13972f65d1c719504062124fd03da95e
Robin Boers <robindev2019@outlook.com>
2024-05-08 19:58:00 +0200

    Bump knockback

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,6 +1,6 @@
 export const G = 2;
 export const FRICTION = 0.1;
 export const SPEED = 8;
-export const KNOCKBACK = 3;
+export const KNOCKBACK = 30;
 export const POWER = 20;
 export const HEALTH = 20;
