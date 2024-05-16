f7cf768d1c2ffbc713dbc7febaf0ce62f80af727
Robin Boers <robindev2019@outlook.com>
2024-05-16 08:14:02 +0200

    Make constants relative to TPS

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,14 +1,21 @@
-export const DURATION = 2 * 60;
 export const FPS = 30;
 export const TPS = 60;
-export const G = 0.6;
-export const MIN_PLAYERS = 2;
-export const FRICTION = 0.8;
-export const SPEED = 1.8;
-export const KNOCKBACK = 30;
-export const POWER = 20;
+export const G = relative(36);
+export const FRICTION = relative(48);
+export const SPEED = relative(108);
+export const KNOCKBACK = relative(1800);
+export const POWER = 30;
 export const HEALTH = 20;
+export const DURATION = 120; // in seconds
+export const MIN_PLAYERS = 2;
 export const LOGGING = false;
+
+// Calculates a value in relation to a single
+// tick. Allows us to change the TPS freely without
+// altering the speed of the game.
+function relative(value: number) {
+  return value/TPS;
+}
 
 // This means the canvas will be rendered at 1/8 the size
 // of the screen and then upscaled, for our retro-pixelation effect.
